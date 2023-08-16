923922634c8e84d8fe712be2af43ab0edd3cd415
Mario <sks_894@hotmail.com>
2023-08-16 07:29:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+function App() {
+  return (
+    <section className='main-container'>
+      <h1 className='title'>Markdown Previewer</h1>
+      <section className='preview-container'>
+        <header className='header'>
+          <h3>Preview</h3>
+          <i>Expand</i>
+        </header>
+        <div id='preview'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tempora porro corporis
+          harum dolor ullam iste ad possimus error enim recusandae optio facilis necessitatibus
+          deleniti nihil ipsa, accusantium inventore soluta. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Nemo esse, aspernatur, reprehenderit nulla deleniti numquam vel modi,
+          sunt qui vitae temporibus at cumque voluptas commodi rerum quis odio exercitationem dolor
+          animi itaque alias. Voluptatem, a molestias vel nesciunt officiis unde sed! Tempora
+          quibusdam earum, beatae animi velit, accusamus officia voluptatibus iste quos, autem in
+          ipsa quod laudantium hic! Dolore consequuntur voluptates beatae quae! Doloribus incidunt
+          ullam est, quo suscipit veritatis ex sapiente aut consequuntur eius totam recusandae quos
+          fugit qui vel deserunt excepturi enim fuga officiis! Corrupti unde nihil enim incidunt
+          perspiciatis est ducimus praesentium in, voluptatibus non obcaecati facere. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Labore tempora porro corporis harum dolor
+          ullam iste ad possimus error enim recusandae optio facilis necessitatibus deleniti nihil
+          ipsa, accusantium inventore soluta. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Nemo esse, aspernatur, reprehenderit nulla deleniti numquam vel modi, sunt qui vitae
+          temporibus at cumque voluptas commodi rerum quis odio exercitationem dolor animi itaque
+          alias. Voluptatem, a molestias vel nesciunt officiis unde sed! Tempora quibusdam earum,
+          beatae animi velit, accusamus officia voluptatibus iste quos, autem in ipsa quod
+          laudantium hic! Dolore consequuntur voluptates beatae quae! Doloribus incidunt ullam est,
+          quo suscipit veritatis ex sapiente aut consequuntur eius totam recusandae quos fugit qui
+          vel deserunt excepturi enim fuga officiis! Corrupti unde nihil enim incidunt perspiciatis
+          est ducimus praesentium in, voluptatibus non obcaecati facere. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Labore tempora porro corporis harum dolor ullam iste ad
+          possimus error enim recusandae optio facilis necessitatibus deleniti nihil ipsa,
+          accusantium inventore soluta. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Nemo esse, aspernatur, reprehenderit nulla deleniti numquam vel modi, sunt qui vitae
+          temporibus at cumque voluptas commodi rerum quis odio exercitationem dolor animi itaque
+          alias. Voluptatem, a molestias vel nesciunt officiis unde sed! Tempora quibusdam earum,
+          beatae animi velit, accusamus officia voluptatibus iste quos, autem in ipsa quod
+          laudantium hic! Dolore consequuntur voluptates beatae quae! Doloribus incidunt ullam est,
+          quo suscipit veritatis ex sapiente aut consequuntur eius totam recusandae quos fugit qui
+          vel deserunt excepturi enim fuga officiis! Corrupti unde nihil enim incidunt perspiciatis
+          est ducimus praesentium in, voluptatibus non obcaecati facere.
+        </div>
+      </section>
+      <section className='editor-container'>
+        <header className='header'>
+          <h3>Editor</h3>
+          <i>Expand</i>
+        </header>
+        <textarea id='editor'></textarea>
+      </section>
+    </section>
+  );
+}
+
+export default App;
